ea669ea955f743f1744d899d745553c69cb35a69
student <student@pc-108-25>
2018-03-21 17:40:08 +0100

    zadania_sroda_21.03

--- POLab4/src/main/java/pl/edu/ur/polab4/Main.java
+++ POLab4/src/main/java/pl/edu/ur/polab4/Main.java
@@ -1,12 +1,29 @@
 package pl.edu.ur.polab4;
+import java.util.Scanner;
 
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
+
+
     public static void main(String[] args) {
         // TODO zad 4, 5
+
+        Scanner s= new Scanner(System.in);
+        Student s1=new Student("Jan");
+        Student s2=new Student("Jan","Mleczko");
+        Student s3=new Student(1231315);
+        Student s4=new Student(1,"Bartosz");
+        String imie = s.nextLine();
+        String nazwisko = s.nextLine();
+        String nazwa_specjalnosci = s.nextLine();
+        int nr_indeks=s.nextInt();
+        int rok_studiow= s.nextInt();
+        Student std = new Student(imie, nazwisko, nr_indeks, nazwa_specjalnosci, rok_studiow);
+        s1.pokazDane();
+        s2.pokazDane();
+        s3.pokazDane();
+        s4.pokazDane();
+
     }
-    
+
 }
